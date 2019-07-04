50346aa23750a044b07717733b31179f239a135a
ameyakulkarni <abkulkar@usc.edu>
2019-07-04 15:30:43 +0530

    Added project category tab layout

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,24 @@
 import React,{Component} from 'react';
-
+import {Tabs,Tab} from 'react-mdl';
 class Projects extends Component{
+
+    state={
+        activeTab:0
+    }
+
     render(){
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="project-category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId)=>this.setState({activeTab:tabId})} ripple>
+                    <Tab>Web Development</Tab>
+                    <Tab>Mobile Development</Tab>
+                    <Tab>Machine Learning and NLP</Tab>
+                    <Tab>Information Retrieval</Tab>
+                </Tabs>
+
+                <section className="project-display-grid">
+                    {this.changeCategories}
+                </section>
             </div>
         );
     }
