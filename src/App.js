281b37e7a54b82c8c70155522c9664e57b93d1b1
ameyakulkarni <abkulkar@usc.edu>
2019-07-04 12:48:34 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
 import './App.css';
-
+import Routes from  './components/routes'
+import {Link} from 'react-router-dom';
 function App() {
   return (
 <div className="demo-big-content">
     <Layout>
         <Header title="Title" scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Routes />
         </Content>
     </Layout>
 </div>
---
+++ src/components/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Homepage from './Homepage'
+import Contact from './Contact'
+import Projects from './Projects'
+import AboutMe from './AboutMe'
+import Resume from './Resume'
+import {Switch,Route} from 'react-router-dom';
+
+const Routes= ()=>(
+    <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/projects" component={Projects} />
+        
+    </Switch>
+)
+
+export default Routes;
